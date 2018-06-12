b77a121bc9cebcd76b8389cd08610407de36fe6e
rosewhitt <rosewhitt@icloud.com>
2018-06-12 18:34:19 +0000

    Done.

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,35 @@
-class Model
-  # Replace with Dog clas
+class Dog
+  def initialize (name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+  end
+
+  def name= (name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def breed= (breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
+  def age=(age)
+    @age = age
+  end
+
+  def age
+    @age
+  end
+
+  def self.all
+    all = [@name]
+  end
 end
