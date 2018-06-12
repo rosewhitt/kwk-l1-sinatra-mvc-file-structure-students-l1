7700121298907da1aa23384c0d49cf32798af619
rosewhitt <rosewhitt@icloud.com>
2018-06-12 18:29:40 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,31 @@
 class Dog
+  def initialize (name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+  end
 
+  def name= (name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def breed= (breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
+  def age=(age)
+    @age = age
+  end
+
+  def age
+    @age
+  end
 end
